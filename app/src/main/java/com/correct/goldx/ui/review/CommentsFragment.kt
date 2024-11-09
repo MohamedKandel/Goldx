ccc252270d33e02e522f71ae6582fee2be0cdc32
MohamedKandel <mohamed.hossam29699@gmail.com>
2024-11-09 22:10:35 +0200

    finish view product ui (write review needed)

---
+++ app/src/main/java/com/correct/goldx/ui/review/CommentsFragment.kt
@@ -0,0 +1,45 @@
+package com.correct.goldx.ui.review
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.correct.goldx.R
+import com.correct.goldx.databinding.FragmentCommentsBinding
+import com.correct.goldx.helper.CastException
+import com.correct.goldx.helper.FragmentChangeListener
+
+class CommentsFragment : Fragment() {
+
+    private lateinit var binding: FragmentCommentsBinding
+    private lateinit var changeListener: FragmentChangeListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is FragmentChangeListener) {
+            changeListener = context
+        } else {
+            throw ClassCastException(CastException)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        changeListener.onFragmentChangeListener(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentCommentsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
